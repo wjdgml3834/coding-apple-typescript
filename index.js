aab2b959c3f4278ac2fe89c8d543018967c66306
junghee.kim <wjdgml3834@naver.com>
2022-02-14 17:26:45 +0900

    📝   3강 타입스크립트 기본 타입 정리

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-// string 타입 지정
-var 이름 = "jung hee";
-// 배열 타입 지정
-var 성 = ["kim", "park"];
-// object 타입 지정
-var 사람 = { name: "정희" };
-// object 타입 지정시 옵션을 주고 싶을때
-var 노트북 = { name: "맥북" };
-// 다양한 타입이 들어오게 하려면 Union type
-var 핸드폰 = "갤럭시";
-var 책 = 123;
-// 함수 타입 지정
-function 함수(x) {
-    return x * 2;
-}
-var TOM = [123, true];
-var ipad = { name: "apple" };
-// class 문법
-var User = /** @class */ (function () {
-    function User(name) {
-        this.name = name;
-    }
-    return User;
-}());
+// 타입 스크립트 기본 타입 정리
+var 이름 = "kim jung hee";
+var 나이 = 27;
+var 결혼했니 = undefined;
+var 담배했니 = null;
+var 회원들 = ["영희", "철수"];
+var 컴퓨터들 = {
+    computer1: "apple",
+    computer2: "samsung",
+};
+// 3강 타입스크립트 과제
+// 1. 나의 이름, 나이, 출생지역을 변수로 저장하기
+var myName = "kim jung hee";
+var myAge = 27;
+var myHomeTown = "서울";
+// 2.내가 가장 좋아하는 곡과 가수 이름을 변수에 object 자료형으로 담아보기
+var myFavorite = {
+    song: "시간을 거슬러",
+    singer: "린",
+};
+// 3. 다음과 같이 생긴 자료의 타입 지정해보기
+// let project = {
+//     member : ['kim', 'park'],
+//     days : 30,
+//     started : true,
+//   }
+//
+var project = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
+};
--- index.ts
+++ index.ts
@@ -1,46 +1,39 @@
-// string 타입 지정
-let 이름: string = "jung hee";
+// 타입 스크립트 기본 타입 정리
 
-// 배열 타입 지정
-let 성: string[] = ["kim", "park"];
-
-// object 타입 지정
-
-let 사람: { name: string } = { name: "정희" };
-
-// object 타입 지정시 옵션을 주고 싶을때
-
-let 노트북: { name?: string } = { name: "맥북" };
-
-// 다양한 타입이 들어오게 하려면 Union type
-let 핸드폰: string | number = "갤럭시";
-
-// 타입을 변수에 담아서 쓸 수도있다. => type alias
-type Mytype = string | number;
-let 책: Mytype = 123;
-
-// 함수 타입 지정
-function 함수(x: number): number {
-  return x * 2;
-}
-
-// array에 쓸 수 있는 tuple 타입
-
-type Member = [number, boolean];
-let TOM: Member = [123, true];
-
-// object에 타입 지정해야할 속성이 많다면?
-type Tablet = {
-  [key: string]: string;
+let 이름: string = "kim jung hee";
+let 나이: number = 27;
+let 결혼했니: undefined = undefined;
+let 담배했니: null = null;
+let 회원들: string[] = ["영희", "철수"];
+let 컴퓨터들: { computer1: string; computer2: string } = {
+  computer1: "apple",
+  computer2: "samsung",
 };
 
-let ipad: Tablet = { name: "apple" };
+// 3강 타입스크립트 과제
 
-// class 문법
+// 1. 나의 이름, 나이, 출생지역을 변수로 저장하기
 
-class User {
-  name: string;
-  constructor(name: string) {
-    this.name = name;
-  }
-}
+let myName: string = "kim jung hee";
+let myAge: number = 27;
+let myHomeTown: string = "서울";
+
+// 2.내가 가장 좋아하는 곡과 가수 이름을 변수에 object 자료형으로 담아보기
+
+let myFavorite: { song: string; singer: string } = {
+  song: "시간을 거슬러",
+  singer: "린",
+};
+
+// 3. 다음과 같이 생긴 자료의 타입 지정해보기
+// let project = {
+//     member : ['kim', 'park'],
+//     days : 30,
+//     started : true,
+//   }
+//
+let project: { member: string[]; days: number; started: boolean } = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
